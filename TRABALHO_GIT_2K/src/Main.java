19cd8409d32a9a5e09ca0129df8e4b68e2955995
Taina Baidek <tainabaidek.ss005@academico.ifsul.edu.br>
2023-02-23 08:12:37 -0300

    Adicionado funcionalidade digitar o nome e imprimir na tela

--- TRABALHO_GIT_2K/src/Main.java
+++ TRABALHO_GIT_2K/src/Main.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Scanner t = new Scanner(System.in);
+        System.out.println("Digite o seu nome: ");
+        String a = t.nextLine();
+        System.out.println("Olá" + a);
 		System.out.println("Olá Mundo");
+		
 	}
 
 }
